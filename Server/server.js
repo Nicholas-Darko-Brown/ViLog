6b1163f654a166a9cb3137bde48947a91a151c15
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-15 23:44:44 +0000

    "added queries to test database connection"

--- Server/server.js
+++ Server/server.js
@@ -11,8 +11,22 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'andreews',
     database: 'vilog_db'
+});
+
+app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO vilog_db.hosts (Full_Name, Email, Position, Phone_number) VALUES ('Ben','ben@gmail.com','Guitarist','024345569')";
+    db.query(sqlInsert, (err, result)=>{
+        res.send("hello");
+    });
+});
+
+app.get("/insert", (req, res) => {
+    const sqlInsert = "INSERT INTO vilog_db.hosts (Full_Name, Email, Position, Phone_number) VALUES ('Benoit','benoit@gmail.com','Guitarist','02434439')";
+    db.query(sqlInsert, (err, result)=>{
+        res.send("helloin");
+    });
 });
 
 //  const port = process.env.PORT || 3001;
