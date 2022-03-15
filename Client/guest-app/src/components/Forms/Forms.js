009bfb33e2306d7e5caa32d37acaa6c54f8438fd
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-15 21:19:10 +0000

    Created forms

--- Client/guest-app/src/components/Forms/Forms.js
+++ Client/guest-app/src/components/Forms/Forms.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 
 const Forms = () => {
   return (
-    <div>Forms, Hello</div>
-  )
-}
+    <Box display="flex" justifyContent='center'>
+      <FormControl
+        isRequired
+        w="30%"
+        // border="2px solid orange.100"
+        p={5}
+        m={5}
+        display="flex"
+        flexDirection="column"
+        borderRadius="1rem"
+        boxShadow="2xl"
+        bg="orange.120"
+      >
+        <Text
+          fontSize="1.2em"
+          fontWeight="extrabold"
+          textTransform="uppercase"
+          letterSpacing="2px"
+          textAlign="center"
+          mb={3}
+        >
+          Tell us about yourself
+        </Text>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input type="text" id="name" placeholder="First name" />
+        <FormLabel htmlFor="company" mt={3}>
+          Company
+        </FormLabel>
+        <Input type="text" id="company" placeholder="Company" />
+        <FormLabel htmlFor="tel" mt={3}>
+          Phone
+        </FormLabel>
+        <Input type="tel" id="tel" placeholder="Phone" />
+        <FormLabel htmlFor="email" mt={3}>
+          Email
+        </FormLabel>
+        <Input type="email" id="email" placeholder="Email" />
+        <Select isRequired placeholder="Select an option" mt={7} >
+          <option value="option1">I am a Visitor</option>
+          <option value="option2">I am a Contractor</option>
+        </Select>
+        <Button colorScheme="orange" variant="solid" mt={9}>
+          Submit
+        </Button>
+      </FormControl>
+    </Box>
+  );
+};
 
-export default Forms
+export default Forms;
