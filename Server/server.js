fd04426978fc0f1f0f9997bba6b9814712708153
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-15 18:35:55 +0000

    "connecting server to database"

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,19 @@
- const express = require("express");
- const cors = require("cors");
- const app = express();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'vilog_db'
+});
 
 //  const port = process.env.PORT || 3001;
 app.listen(3001, () => {
