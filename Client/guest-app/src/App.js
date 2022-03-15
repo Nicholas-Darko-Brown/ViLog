3f89c969bc8c2c4b74f42602be85f4d222336bc7
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-15 18:26:35 +0000

    Created Form structure

--- Client/guest-app/src/App.js
+++ Client/guest-app/src/App.js
@@ -4,12 +4,16 @@ import {
   Box
 } from '@chakra-ui/react';
 import Home from './components/Home/Home';
+import Forms from './components/Forms/Forms'
 
 function App() {
   return (
     <ChakraProvider>
       <Box fontSize="xl">
         <Home />
+      </Box>
+      <Box>
+        <Forms />
       </Box>
     </ChakraProvider>
   );
