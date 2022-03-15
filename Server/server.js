03b01603b880c7b720c28714e6f004b6cbc8da26
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-15 14:51:55 +0000

    "setting the server"

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,8 @@
- 
+ const express = require("express");
+ const cors = require("cors");
+ const app = express();
+
+//  const port = process.env.PORT || 3001;
+app.listen(3001, () => {
+    console.log("Server listening on port 3001");
+});
