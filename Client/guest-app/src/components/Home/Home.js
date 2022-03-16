35f4ff78e89f14865cef6cd3b9a700df9abb68e2
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-16 15:20:55 +0000

    Added Dashboard button

--- Client/guest-app/src/components/Home/Home.js
+++ Client/guest-app/src/components/Home/Home.js
@@ -1,30 +1,55 @@
 import React from 'react';
-import { Box, Flex, Spacer, Image, Text, } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Spacer,
+  Image,
+  Text,
+  Button,
+  Center,
+  Heading
+} from '@chakra-ui/react';
 import Logo from '../../assets/logo-1.png';
+import { MdDashboard } from 'react-icons/md';
 
 const Home = () => {
   return (
-    <Box height='100vh'>
+    <Box height="100vh">
       <Flex
         flexDirection="column"
-        justifyContent="center"
+        // justifyContent="center"
         alignItems="center"
         gap="2rem"
         // mt="2rem"
       >
-        <Box p="4" display="flex" alignItems="center">
-          <Image src={Logo} alt="logo" />
-          <Text fontSize="2em" fontWeight="extrabold">
-            ViLog
-          </Text>
+        <Box
+          p="0 1rem"
+          w="70%"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box display="flex" alignItems="center">
+            <Image src={Logo} alt="logo" />
+            <Text fontSize="2em" fontWeight="extrabold">
+              ViLog
+            </Text>
+          </Box>
+          <Box>
+            <Button
+              rightIcon={<MdDashboard />}
+              colorScheme="orange"
+              variant="outline"
+              fontWeight="bold"
+            >
+              Dashboard
+            </Button>
+          </Box>
         </Box>
         <Spacer />
-        <Box p="4">
-          <Text textTransform="uppercase" fontSize="2em" letterSpacing='2px' fontWeight='bold'>
-            Welcome...
-          </Text>
-        </Box>
-        <Spacer />
+        <Center h="300px">
+        <Heading size='4xl'>Welcome...</Heading>
+        </Center>
         {/* <Box p="4">
           <Button p="8" colorScheme="orange" size="lg">
             Click to start
