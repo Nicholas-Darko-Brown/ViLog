62423b1c7c9413c64920c2c8b636de573fe8e410
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-17 14:54:16 +0000

    "added route for saving visitor 's data"

--- Server/server.js
+++ Server/server.js
@@ -11,9 +11,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json())
 
+const visitorsTable = tables.visitors.name;
+const {select, insertInto, values} = sql_keywords;
+const {fullNameCol, companyCol, phoneNumberCol, emailCol, hostCol, positionCol} = tables.visitors.colums;
+
 
 app.post("/", (req, res) => {
-    
+    const {name, company, tel, email, position, host} = req.body;
+    const insertVisitorQuery = `${insertInto} ${visitorsTable} (${fullNameCol}, ${companyCol}, ${phoneNumberCol}, ${emailCol}, ${hostCol}, ${positionCol}) ${values} (?,?,?,?,?,?)`;
+
+    db.query(insertVisitorQuery, [name, company, tel, email, host, position], (err, result) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("visitor added.");
+            res.status(201).send();
+        }
+    });
 });
 
 //  const port = process.env.PORT || 3001;
