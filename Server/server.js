bf7c83d7be9e877eb83520e505e6200b4085c379
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-18 09:42:18 +0000

    "Added a middleware"

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,7 @@ const tables = require("./config/tables");
 const app = express();
 const port = process.env.PORT || 3000;
 //app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json())
 
