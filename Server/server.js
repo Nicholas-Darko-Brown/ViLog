2e9cb7dca0e877b630ca1bcfd9485dc6e6fc71f6
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-03-19 02:23:29 +0000

    "server port number changed"

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const db = require("./config/database_connection");
 const sql_keywords = require("./config/sql_keywords");
 const tables = require("./config/tables");
-
+dotenv.config({path:'../.env'});
 const app = express();
-const port = process.env.PORT || 3000;
-//app.use(cors());
+const port = process.env.PORT || 5000;
+console.log(process.env.PORT);
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json());
