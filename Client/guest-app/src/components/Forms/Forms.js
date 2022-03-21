0193820a6d1b1d97a8ee47ee1fcd47ca9f04fde7
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-21 00:53:17 +0000

    Updated

--- Client/guest-app/src/components/Forms/Forms.js
+++ Client/guest-app/src/components/Forms/Forms.js
@@ -7,60 +7,61 @@ import {
   Text,
   Box,
 } from '@chakra-ui/react';
-import Axios from 'axios'
+import Axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './Forms.css'
 
 const Forms = () => {
-  const [employee, setEmployee] = useState([])
+  const [employee, setEmployee] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
 
-
-  const url = "http://localhost:3000/"
+  const url = 'http://localhost:3000/';
   const [data, setData] = useState({
-    name: "",
-    company: "",
-    tel: "",
-    email: "",
-    position: "",
-    host: ""
-  })
-  console.log(data)
-  
+    name: '',
+    company: '',
+    tel: '',
+    email: '',
+    position: '',
+    host: '',
+  });
+  console.log(data);
+
 
   const handleChange = e => {
-    const newData = {...data}
-    newData[e.target.id] = e.target.value
-    setData(newData)
-    console.log(newData)
-  }
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  };
 
   const handleSubmit = e => {
-    console.log("submitted")
-    e.preventDefault()
-    console.log(data)
-    
-    Axios.post(url,data)
-  }
+    console.log('submitted');
+    e.preventDefault();
+    console.log(data);
+
+    Axios.post(url, data);
+  };
 
   const fetchEmployees = async () => {
-    const { data } = await Axios.get("/employeeName");
+    const { data } = await Axios.get('/employeeName');
     setEmployee(data);
-  }
-  
+  };
+
   useEffect(() => {
-    fetchEmployees()
+    fetchEmployees();
   }, []);
-  
- 
+
   return (
-    <Box display="flex" justifyContent='center' height='100%'>
+    <Box display="flex" justifyContent="center" height="100%">
       <FormControl
-        
         isRequired
         w="30%"
         p={5}
         m={5}
         display="flex"
         flexDirection="column"
-        justifyContent='center'
+        justifyContent="center"
         borderRadius="1rem"
         boxShadow="2xl"
         bg="orange.120"
@@ -76,24 +77,87 @@ const Forms = () => {
           Tell us about yourself
         </Text>
 
-        <Input onChange={(e) => handleChange(e)} value={data.name} type="text" id="name" placeholder="Name" />
-        <Input onChange={(e) => handleChange(e)} value={data.company} type="text" id="company" mt={5} placeholder="Company" />
-        <Input onChange={(e) => handleChange(e)} value={data.tel} type="tel" id="tel" mt={5} placeholder="Phone" />
-        <Input onChange={(e) => handleChange(e)} value={data.email} type="email" id="email" mt={5} placeholder="Email" />
+        <Input
+          onChange={e => handleChange(e)}
+          value={data.name}
+          type="text"
+          id="name"
+          placeholder="Name"
+        />
+        <Input
+          onChange={e => handleChange(e)}
+          value={data.company}
+          type="text"
+          id="company"
+          mt={2}
+          placeholder="Company"
+        />
+        <Input
+          onChange={e => handleChange(e)}
+          value={data.tel}
+          type="tel"
+          id="tel"
+          mt={2}
+          placeholder="Phone"
+        />
+        <Input
+          onChange={e => handleChange(e)}
+          value={data.email}
+          type="email"
+          id="email"
+          mt={2}
+          placeholder="Email"
+        />
 
-        <Select onChange={(e) => handleChange(e)} value={data.position} isRequired id="position" placeholder="Select your position" mt={5} >
+        <Select
+          onChange={e => handleChange(e)}
+          value={data.position}
+          isRequired
+          id="position"
+          placeholder="Select your position"
+          mt={2}
+        >
           <option value="visitor">Visitor</option>
           <option value="contractor">Contractor</option>
         </Select>
-        <Select onChange={(e) => handleChange(e)} value={data.host} isRequired id="host" placeholder="Who are you visiting?" mt={5} >
-          { employee? employee.map((name) => {
-            return <option value={name.Full_Name}>{name.Full_Name}</option>
-          }) : <option value="yvonne smith">Yvonne Smith</option> }
-          
+        <Select
+          onChange={e => handleChange(e)}
+          value={data.host}
+          isRequired
+          id="host"
+          placeholder="Who are you visiting?"
+          mt={2}
+        >
+          {employee ? (
+            employee.map(name => {
+              return <option value={name.Full_Name}>{name.Full_Name}</option>;
+            })
+          ) : (
+            <option value="yvonne smith">Yvonne Smith</option>
+          )}
+
           {/*  */}
         </Select>
 
-        <Button onClick={(e) => handleSubmit(e)} type="submit" colorScheme="orange" variant="solid" mt={9}>
+        <DatePicker
+          selected={selectedDate}
+          onChange={date => setSelectedDate(date)}
+          dateFormat="MMMM d, yyyy h:mm aa"
+          minDate={new Date()}
+          filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+          showYearDropdown
+          showTimeSelect
+          placeholderText="Please, select your check in time"
+          className='date_picker'
+        />
+
+        <Button
+          onClick={e => handleSubmit(e)}
+          type="submit"
+          colorScheme="orange"
+          variant="solid"
+          mt={9}
+        >
           Submit
         </Button>
       </FormControl>
