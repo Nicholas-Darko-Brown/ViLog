8647e05fe61cf5ed731ef6c3e5c6586fc2b6d41c
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-22 10:46:41 +0000

    Edited

--- Client/guest-app/src/index.js
+++ Client/guest-app/src/index.js
@@ -3,14 +3,11 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { AuthProvider } from './Context/AuthProvider';
 
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </StrictMode>,
   document.getElementById('root')
 );
