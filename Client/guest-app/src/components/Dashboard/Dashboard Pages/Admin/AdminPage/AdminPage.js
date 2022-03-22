ae4c25f0e7159839da1095cecfc993b63c35a4a6
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-03-22 17:09:40 +0000

    Created tables

--- Client/guest-app/src/components/Dashboard/Dashboard Pages/Admin/AdminPage/AdminPage.js
+++ Client/guest-app/src/components/Dashboard/Dashboard Pages/Admin/AdminPage/AdminPage.js
@@ -1,119 +1,30 @@
-import { useState } from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import { DeleteOutline } from '@material-ui/icons';
+import { useState, useEffect } from 'react';
 import Navbar from '../../../Dashboard Components/Navbar/Navbar';
 import Sidebar from '../../../Dashboard Components/Sidebar/Sidebar';
 import './AdminPage.css';
-import { hostRows } from '../../../../../DummyData';
-import { useNavigate } from 'react-router-dom';
-import { columnRows } from '../../../../../DummyData';
+import axios from 'axios'
 
 const AdminPage = () => {
-  let navigate = useNavigate();
+  const [visitors, setVisitors] = useState([])
+  const [employees, setEmployees] = useState([])
 
-  const [data, setData] = useState(hostRows);
-  const [employee, setEmployee] = useState(columnRows)
-
-  const handleDelete = id => {
-    setData(data.filter(item => item.id !== id));
-  };
-
-  const handleDeleteEmployee = id => {
-    setEmployee(employee.filter(item => item.id !== id))
+  const fetchVisitors = async () => {
+    const { data } = await axios.get("/adminPage/visitorsLog")
+    setVisitors(data)
   }
 
-  const visitorColumns = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'name', headerName: 'Visitor', width: 130 },
-    { field: 'email', headerName: 'Email', width: 180 },
-    {
-      field: 'company',
-      headerName: 'Company',
-      width: 150,
-    },
-    {
-      field: 'host',
-      headerName: 'Host',
-      sortable: true,
-      width: 150,
-    },
-    {
-      field: 'sign_in',
-      headerName: 'Sign In',
-      sortable: true,
-      width: 150,
-    },
-    {
-      field: 'sign_out',
-      headerName: 'Sign Out',
-      sortable: true,
-      width: 150,
-    },
-    {
-      field: 'status',
-      headerName: 'Status',
-      sortable: false,
-      width: 130,
-    },
-    {
-      field: 'action',
-      headerName: 'Action',
-      width: 150,
-      renderCell: params => {
-        return (
-          <div className="admin_action_container">
-            {/* <Link to={"/edit/"+params.row.id}> */}
-            <button
-              className="admin_edit"
-              onClick={() => {
-                navigate('/edit/' + params.row.id);
-              }}
-            >
-              Edit
-            </button>
-            {/* </Link> */}
-            <DeleteOutline
-              className="admin_delete"
-              onClick={() => handleDelete(params.row.id)}
-            />
-          </div>
-        );
-      },
-    },
-  ];
+  const fetchEmployees = async () => {
+    const { data } = await axios.get("/adminPage/employeeList")
+    setEmployees(data)
+  }
 
-  const employeeColumns = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'email', headerName: 'Email', width: 180 },
-    { field: 'phone', headerName: 'Phone', width: 120 },
-    { field: 'position', headerName: 'Position', width: 150 },
-    {
-      field: 'action',
-      headerName: 'Action',
-      width: 150,
-      renderCell: params => {
-        return (
-          <div className="admin_action_container">
-            {/* <Link to={"/edit/"+params.row.id}> */}
-            <button
-              className="admin_edit"
-              onClick={() => {
-                navigate('/employeeEdit/' + params.row.id);
-              }}
-            >
-              Edit
-            </button>
-            {/* </Link> */}
-            <DeleteOutline
-              className="admin_delete"
-              onClick={() => handleDeleteEmployee(params.row.id)}
-            />
-          </div>
-        );
-      },
-    },
-  ];
+  useEffect(() => {
+    fetchVisitors()
+  }, [])
+
+  useEffect(() => {
+    fetchEmployees()
+  }, [])
 
   return (
     <div className="admin_container">
@@ -121,27 +32,59 @@ const AdminPage = () => {
       <div className="admin_sidebar">
         <Sidebar />
         <div className="admin_content">
-          <span className="admin_content_title">Admin Visitor Records</span>
-          <DataGrid
-            rows={data}
-            columns={visitorColumns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            disableSelectionOnClick
-            className='visitor_records'
-          />
+        <span className='table_title'>Admin Visitor Data</span>
+          <table>
+            <thead>
+              <tr>
+                <th>Visitor Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Company</th>
+                <th>Position</th>
+                <th>Host</th>
+                <th>Time in</th>
+                <th>Time out</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+            {visitors.map((visitor) => (
+              <tr>
+                <td>{ visitor.Full_name }</td>
+                <td>{ visitor.Email }</td>
+                <td>{ visitor.Phone_Number }</td>
+                <td>{ visitor.Company }</td>
+                <td>{ visitor.Position }</td>
+                <td>{ visitor.Host }</td>
+                <td>{ visitor.Time_In }</td>
+                <td>{ visitor.Tiem_Out }</td>
+              </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <span>Employee Data</span>
+          <table>
+            <thead>
+              <tr>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Position</th>
+              </tr>
+            </thead>
+            <tbody>
+            {employees.map((employee) => (
+              <tr>
+                <td>{ employee.Full_Name }</td>
+                <td>{ employee.Email }</td>
+                <td>{ employee.Phone_Number }</td>
+                <td>{ employee.Position }</td>
+              </tr>
+              ))}
+            </tbody>
+          </table>
           
-          <span className="employee_content_title">Employee Records</span>
-          <DataGrid
-            rows={employee}
-            columns={employeeColumns}
-            pageSize={10}
-            // rowsPerPageOptions={[7]}
-            checkboxSelection
-            disableSelectionOnClick
-            className='employee_records'
-          />
         </div>
       </div>
     </div>
